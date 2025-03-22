9a28e7a7d51decbaaa7f6902aba53064a41a1710
Subrato Kumar Verma <138965935+18subrato@users.noreply.github.com>
2025-03-22 20:34:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,41 @@
-//your JS code here. If required.
+ let div = document.querySelector("#output")
+        let inputBox = document.querySelector("#ip")        
+
+        function handleStart(){
+            let val = inputBox.value; 
+            new Promise(function(res,rej){
+                setTimeout(()=>{
+                    div.innerText = `Result:${val}`
+                    res(val*2)
+                },2000)
+            })
+            .then((val)=>{
+                return new Promise(function(res,rej){
+                    setTimeout(()=>{
+                        div.innerText = `Result:${val}`
+                        res(val-3)
+                    },3000)
+                })
+            })
+            .then((val)=>{
+                return new Promise(function(res,rej){
+                    setTimeout(()=>{
+                        div.innerText = `Result:${val}`
+                        res(val/2)
+                    },4000)
+                })
+            })
+            .then((val)=>{
+                return new Promise(function(res,rej){
+                    setTimeout(()=>{
+                        div.innerText = `Result:${val}`
+                        res(val+10)
+                    },5000)
+                })
+            })
+            .then((val)=>{
+                setTimeout(()=>{
+                    div.innerText = `Final Result:${val}`
+                },6000)
+            })
+        }
