4481e4d4247a9c78efcadab92687705cde28f1fb
Subrato Kumar Verma <138965935+18subrato@users.noreply.github.com>
2025-03-22 20:39:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
  let div = document.querySelector("#output")
         let inputBox = document.querySelector("#ip")        
-
+	
         function handleStart(){
-            let val = inputBox.value; 
+            let val = inputBox.value;
+			if(val === 1){
+				val = 5;
+			} 
+
+			if(val === 2){
+				val = 10
+			}
             new Promise(function(res,rej){
                 setTimeout(()=>{
                     div.innerText = `Result:${val}`
